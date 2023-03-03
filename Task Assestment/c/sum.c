ae170cdca24a82b7622fd5e88f76bc4f05db33ee
GadaDarshan <gadadarshan926@gmail.com>
2023-03-03 14:14:44 +0530

    Completed
    Completed

---
+++ Task Assestment/c/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n, sum=0, firstDigit, lastDigit,digit;
+    printf("Enter number to find sum of first and last digit = ");
+    scanf("%d", &n);
+    lastDigit = n % 10;
+    digit    = (int)log10(n);
+    firstDigit = (int) (n / pow(10, digit));
+    sum = firstDigit + lastDigit;
+    printf("Sum of first and last digit = %d", sum);
+    return 0;
+}
